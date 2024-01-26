32c48b3fe773c3f90176d044a29fa1f3e2770aa5
ylin <17835344407@163.com>
2024-01-26 14:39:42 +0800

    feat: use form instead of body

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use axum::{
-    body,
     extract::Request,
     http::{header, StatusCode},
-    routing, Json, Router,
+    routing, Form, Json, RequestExt, Router,
 };
 use qasmsim;
+use serde::Deserialize;
 use serde_json::{json, Value};
+
+#[derive(Deserialize)]
+struct EmulateMessage {
+    qasm: String,
+    shots: usize,
+    format: String,
+}
 
 pub async fn root() -> (StatusCode, Json<Value>) {
     let source = "
@@ -36,51 +43,18 @@ pub async fn root() -> (StatusCode, Json<Value>) {
     }
 }
 
-pub async fn consume_body(body: body::Body) -> (StatusCode, Json<Value>) {
-    let body_str = match body::to_bytes(body, usize::MAX).await {
-        Ok(bytes) => match String::from_utf8(bytes.to_vec()) {
-            Ok(string) => string,
-            Err(err) => {
-                return (
-                    StatusCode::BAD_REQUEST,
-                    Json(json!({"Error": format!("{}", err)})),
-                )
-            }
-        },
-        Err(err) => {
-            return (
-                StatusCode::BAD_REQUEST,
-                Json(json!({"Error": format!("{}", err)})),
-            )
-        }
-    };
-
-    let parts = body_str.split("&").collect::<Vec<&str>>();
+async fn consume_body(Form(emulate_message): Form<EmulateMessage>) -> (StatusCode, Json<Value>) {
     let options = qasmsim::options::Options {
-        shots: match parts[1].split("=").collect::<Vec<&str>>()[1].parse::<usize>() {
-            Ok(shots) => Some(shots),
-            Err(err) => {
-                return (
-                    StatusCode::BAD_REQUEST,
-                    Json(json!({"Error": format!("{}", err)})),
-                )
-            }
-        },
-        format: match parts[2].split("=").collect::<Vec<&str>>()[1] {
+        shots: Some(emulate_message.shots),
+        format: match emulate_message.format.as_str() {
             "json" => qasmsim::options::Format::Json,
-            "csv" => qasmsim::options::Format::Tabular,
-            _ => {
-                return (
-                    StatusCode::BAD_REQUEST,
-                    Json(json!(
-                        {"Error": "format not specified / not support"})),
-                )
-            }
+            "tabular" => qasmsim::options::Format::Tabular,
+            _ => qasmsim::options::Format::Json,
         },
         ..Default::default()
     };
 
-    match qasmsim::run(parts[0], options.shots) {
+    match qasmsim::run(&emulate_message.qasm, options.shots) {
         Ok(result) => (
             StatusCode::OK,
             Json(serde_json::from_str::<Value>(&qasmsim::print_result(&result, &options)).unwrap()),
@@ -95,12 +69,15 @@ pub async fn consume_body(body: body::Body) -> (StatusCode, Json<Value>) {
 pub async fn emulate(request: Request) -> (StatusCode, Json<Value>) {
     // curl -v -H "Content-Type: x-www-form-urlencoded" -X POST 10.31.4.69:3000/emulate -d @bell.qasm -d shots=1000 -d format=json
     match request.headers().get(header::CONTENT_TYPE) {
-        Some(content_type) if content_type == "x-www-form-urlencoded" => {
-            consume_body(request.into_body()).await
+        Some(content_type) if content_type == "application/x-www-form-urlencoded" => {
+            let Form(emulate_message) = request.extract().await.unwrap();
+            consume_body(Form(emulate_message)).await
         }
         _ => (
             StatusCode::BAD_REQUEST,
-            Json(json!({"Error": "content type not specified / not support"})),
+            Json(
+                json!({"Error": format!("content type {:?} not specified / not support", request.headers().get(header::CONTENT_TYPE).unwrap())}),
+            ),
         ),
     }
 }
